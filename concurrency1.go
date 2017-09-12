7c9c70146ce4b57a4ef831ac0aa81084df43afe4
drjak <drjak@wp.pl>
2017-09-12 20:20:38 +0100

    concurency

---
+++ concurrency1.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func f(n int){
+	for i:=0;i<10;i++  {
+		fmt.Println(n,":",i)
+	}
+}
+func main(){
+	for i:=0;i<3;i++{
+		go f(i)
+	}
+	var input string
+	fmt.Scanln(&input)
+}
