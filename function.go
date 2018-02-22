36021f6ffa8fbeb2de79b8b0f5b136f3392bdcc3
Jarek Kalinowski <jaroslaw.kalinowski@sky.uk>
2018-02-22 21:22:31 +0000

    changes

--- function.go
+++ function.go
@@ -1,19 +1,78 @@
 package main
 
-import (
-	"fmt"
-)
-func average(xs []float64)float64{
-	total := 0.0
-	for _, a := range xs{
-		total += a
-	}
-	return total / float64(len(xs))
-}
-func main(){
-	xs := []float64{54,56,56,65,78}
-	fmt.Println(average(xs))
+import "fmt"
 
-	some_other := []float64{45,657,34,65,7}
-	fmt.Println(average(some_other))
+func add(args ...int)int{
+	total := 0
+	for _, v := range args  {
+		total+=v
+	}
+	return total
+}
+func average(xs []float64)float64{
+        total := 0.0
+        for _, a := range xs{
+                total += a
+        }
+        return total / float64(len(xs))
+}
+
+//function that return other function
+func makeEvenGenerator()func()uint{
+	i := uint(0)
+	return func() (ret uint){
+		ret = 1
+		i += 2
+		return
+		}
+	}
+
+//deffering functions
+func first(){
+	fmt.Println("1st")
+	}
+func second(){
+	fmt.Println("2nd")
+	}
+
+func main(){
+	fmt.Println("simple adding",add(1,2,4))
+	//you can also pass slice
+	slice := []int{1,2,3}
+	fmt.Println("slice: ", add(slice...))
+
+        //using function average
+        xs := []float64{54,56,56,65,78}
+        fmt.Println(average(xs))
+
+        some_other := []float64{45,657,34,65,7}
+        fmt.Println(average(some_other))
+	
+	//function inside the function
+	sum := func(x,y int)int{
+		return x+y
+		}
+		//add is a local variable that has a type func(int, int)int
+		//when created local functions like this it has access to other local variables
+	fmt.Println("function inside function, sum",sum(1,6))
+
+ 	x := 0
+	increment := func()int{
+		x++
+		return x
+		}
+	fmt.Println("increment", increment())
+	fmt.Println("increment", increment())
+        
+	//return other function test
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	
+	//deffering
+	fmt.Println("testing deffering")
+	defer second()
+	first()
+
 }
--- nigel/level2_2.go
+++ nigel/level2_2.go
@@ -1,37 +1,34 @@
 package main
 
 import (
-        "fmt"
-        "net/http"
-        "os"
+	"fmt"
+	"net/http"
+	"os"
 )
-func check_args(){
-		if len(os.Args) < 2{
-			fmt.Println("Please provide URL")
-			os.Exit(2)
+
+func check_args() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide URL")
+		os.Exit(2)
 	}
 }
 
-func check_res(url string){
-	
-	        req, err := http.Get(url)
-		    if err !=nil {
-	        	fmt.Print("Some error")
-	        	os.Exit(2)
-	        }
+func check_res(url string) {
 
-		code := req.StatusCode
-        fmt.Println("Reponse", code)
+	req, err := http.Get(url)
+	if err != nil {
+		fmt.Print("Some error")
+		os.Exit(2)
+	}
+
+	code := req.StatusCode
+	fmt.Println("Reponse", code)
 }
 
 func main() {
-
-url := []string{"http://wp.pl", "http://google.com", "http://bbc.com"}
-for i := 0; i<len(url); i++ {
-	fmt.Println("site ",url[i])
-	check_res(url[i])
-}
-        
-
-        
+	url := []string{"http://wp.pl", "http://google.com", "http://bbc.com"}
+	for i := 0; i < len(url); i++ {
+		fmt.Println("site ", url[i])
+		check_res(url[i])
+	}
 }
